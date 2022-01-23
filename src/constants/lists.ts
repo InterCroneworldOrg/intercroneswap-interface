04c74876f3391ebada62dc7158e35ef58bc07add
Partha Dhar <partha.dhar2008@gmail.com>
2022-01-23 14:03:25 +0530

    removed dev link

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the ISwap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://dev.intercroneswap.com/troniswap.json';
+export const DEFAULT_TOKEN_LIST_URL = 'https://intercroneswap.com/troniswap.json';
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
