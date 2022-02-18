24156ce41b54e6d9f35bf1c10a3628130107edcf
Gabor Nagy <gaborka19890603@gmail.com>
2022-02-18 13:09:38 +0100

    Switch to working router and factory

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector';
 import { injected } from '../connectors';
 
 // export const ROUTER_ADDRESS = '0x093a9fa8b6bb0395fd2568f11a9325d882a69b55';
-export const ROUTER_ADDRESS = '0xFFFBE2416840E1E3B9E052939E2D057D97FA9655';
+export const ROUTER_ADDRESS = '0xf8351aa88c23b5d34c4a2d625a6912a9476ee770';
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 // a list of tokens by chain
