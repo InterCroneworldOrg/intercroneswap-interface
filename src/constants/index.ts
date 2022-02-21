37ba274a9d4dc6729700ca9feba2745e2252284a
Gabor Nagy <gaborka19890603@gmail.com>
2022-02-21 17:51:30 +0100

    Update to latest router

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector';
 import { injected } from '../connectors';
 
 // export const ROUTER_ADDRESS = '0x093a9fa8b6bb0395fd2568f11a9325d882a69b55';
-export const ROUTER_ADDRESS = '0xf8351aa88c23b5d34c4a2d625a6912a9476ee770';
+export const ROUTER_ADDRESS = '0x0c7da363814e5e93582bcabc74affdd5037d5716';
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 
 // a list of tokens by chain
