00262afda5f5481b1c57ca3aeffa0f3a65a21e8c
Gabor Nagy <gaborka19890603@gmail.com>
2022-03-06 09:13:24 +0100

    Fix token logo errors, add extra format types

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -10,11 +10,15 @@ import { ethAddress } from '@intercroneswap/java-tron-provider';
 import { useAllLists } from '../../state/lists/hooks';
 import { TokenList } from '@intercroneswap/token-lists';
 
-const getTokenLogoURL = (address: string, allTokens: TokenList[]): string => {
-  return (
-    allTokens.flatMap((tokens) => tokens.tokens).find((token) => token.address.toLowerCase() === address.toLowerCase())
-      ?.logoURI ?? `https://coin.top/production/upload/logo/${ethAddress.toTron(address)}.png`
-  );
+const coinTopFormats = ['png', 'jpeg', 'jpg'];
+
+const getTokenLogoURL = (address: string, allTokens: TokenList[]): string[] => {
+  const default_list_logo = allTokens
+    .flatMap((tokens) => tokens.tokens)
+    .find((token) => token.address.toLowerCase() === address.toLowerCase())?.logoURI;
+  const coin_top_main_url = `https://coin.top/production/upload/logo/${ethAddress.toTron(address)}.`;
+  const allCoinTopFormats = coinTopFormats.map((format) => `${coin_top_main_url}${format}`);
+  return default_list_logo ? [default_list_logo] : allCoinTopFormats;
 };
 
 const StyledEthereumLogo = styled.img<{ size: string }>`
@@ -48,10 +52,10 @@ export default function CurrencyLogo({
 
     if (currency instanceof Token) {
       if (currency instanceof WrappedTokenInfo) {
-        return [...uriLocations, getTokenLogoURL(currency.address, allTokens)];
+        return [...uriLocations, ...getTokenLogoURL(currency.address, allTokens)];
       }
 
-      return [getTokenLogoURL(currency.address, allTokens)];
+      return [...getTokenLogoURL(currency.address, allTokens)];
     }
     return [];
   }, [currency, uriLocations]);
