5a8c64af43130b1839e464800e70f0eb04b369fc
Manntrix <Manish.mandal21@gmail.com>
2022-04-05 19:52:25 +0530

    header and footer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,9 @@ import { Text } from 'rebass';
 import { NavLink } from 'react-router-dom';
 import { darken } from 'polished';
 import { useTranslation } from 'react-i18next';
-
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import Style from '../../styles/header.module.css';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import Logo from '../../assets/images/ISwap.svg';
 // import Logo from '../../assets/svg/logo.svg'
@@ -17,6 +19,7 @@ import Menu from '../Menu';
 import { Box } from 'rebass/styled-components';
 import Web3Status from '../Web3Status';
 import Modal from '../Modal';
+import EthLogo from '../../assets/images/eth-logo.png';
 
 const HeaderFrame = styled.div`
   // display: grid;
@@ -366,76 +369,137 @@ export default function Header() {
 
   // TODO: Redesign HEADER
   return (
-    <HeaderFrame>
-      {/* <ClaimModal /> */}
-      <Modal isOpen={showIswapBalanceModal} onDismiss={() => setShowIswapBalanceModal(false)}></Modal>
-      <HeaderRow>
-        <Title href=".">
-          <IswapIcon>
+    // <HeaderFrame>
+    //   {/* <ClaimModal /> */}
+    //   <Modal isOpen={showIswapBalanceModal} onDismiss={() => setShowIswapBalanceModal(false)}></Modal>
+    //   <HeaderRow>
+    //     <Title href=".">
+    //       <IswapIcon>
+    //         <img width={'115px'} src={Logo} alt="logo" />
+    //       </IswapIcon>
+    //     </Title>
+    //     <StyledHeaderElements>
+    //       <LinksContainer>
+    //         <StyledNavLink id={`swap-nav-link`} to={'/swap'}>
+    //           {t('swap')}
+    //         </StyledNavLink>
+    //       </LinksContainer>
+    //       <StyledNavLink
+    //         id={`pool-nav-link`}
+    //         to={'/pool'}
+    //         isActive={(match, { pathname }) =>
+    //           Boolean(match) ||
+    //           pathname.startsWith('/add') ||
+    //           pathname.startsWith('/remove') ||
+    //           pathname.startsWith('/create') ||
+    //           pathname.startsWith('/find')
+    //         }
+    //       >
+    //         {t('pool')}
+    //       </StyledNavLink>
+    //       <LinksContainer>
+    //         <StyledNavLink id={`stake-nav-link`} to={'/stake'}>
+    //           {t('Stake')}
+    //         </StyledNavLink>
+    //       </LinksContainer>
+    //     </StyledHeaderElements>
+    //     <HeaderLinks>
+    //       <AccountElement active={!!account} style={{ pointerEvents: 'auto' }}>
+    //         {account && userEthBalance ? (
+    //           <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
+    //             {userEthBalance?.toSignificant(4)} TRX
+    //           </BalanceText>
+    //         ) : null}
+    //         <Web3Status />
+    //       </AccountElement>
+    //       <Settings />
+    //       {/* <LeftMenu /> */}
+    //       <Menu />
+    //     </HeaderLinks>
+    //   </HeaderRow>
+    //   {/* <HeaderControls>
+    //     <HeaderElement>
+    //       <HideSmall>
+    //         {chainId && NETWORK_LABELS[chainId] && (
+    //           <NetworkCard title={NETWORK_LABELS[chainId]}>{NETWORK_LABELS[chainId]}</NetworkCard>
+    //         )}
+    //       </HideSmall> */}
+    //   {/* {availableClaim && !showClaimPopup} */}
+    //   {/* <AccountElement active={!!account} style={{ pointerEvents: 'auto' }}>
+    //         {account && userEthBalance ? (
+    //           <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
+    //             {userEthBalance?.toSignificant(4)} TRX
+    //           </BalanceText>
+    //         ) : null}
+    //         <Web3Status />
+    //       </AccountElement>
+    //     </HeaderElement>
+    //     <HeaderElementWrap>
+    //       <Settings />
+    //       <Menu />
+    //     </HeaderElementWrap>
+    //   </HeaderControls> */}
+    // </HeaderFrame>
+    <header id="mainheader">
+      <Navbar
+        expand="lg"
+        style={{ width: '100%', background: 'linear-gradient(180deg, #3B3B3B 0%, rgba(59, 59, 59, 0) 100%)' }}
+      >
+        <Container fluid>
+          <Navbar.Brand href="#home">
             <img width={'115px'} src={Logo} alt="logo" />
-          </IswapIcon>
-        </Title>
-        <StyledHeaderElements>
-          <LinksContainer>
-            <StyledNavLink id={`swap-nav-link`} to={'/swap'}>
-              {t('swap')}
-            </StyledNavLink>
-          </LinksContainer>
-          <StyledNavLink
-            id={`pool-nav-link`}
-            to={'/pool'}
-            isActive={(match, { pathname }) =>
-              Boolean(match) ||
-              pathname.startsWith('/add') ||
-              pathname.startsWith('/remove') ||
-              pathname.startsWith('/create') ||
-              pathname.startsWith('/find')
-            }
-          >
-            {t('pool')}
-          </StyledNavLink>
-          <LinksContainer>
-            <StyledNavLink id={`stake-nav-link`} to={'/stake'}>
-              {t('Stake')}
-            </StyledNavLink>
-          </LinksContainer>
-        </StyledHeaderElements>
-        <HeaderLinks>
-          <AccountElement active={!!account} style={{ pointerEvents: 'auto' }}>
-            {account && userEthBalance ? (
-              <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
-                {userEthBalance?.toSignificant(4)} TRX
-              </BalanceText>
-            ) : null}
-            <Web3Status />
-          </AccountElement>
-          <Settings />
-          {/* <LeftMenu /> */}
-          <Menu />
-        </HeaderLinks>
-      </HeaderRow>
-      {/* <HeaderControls>
-        <HeaderElement>
-          <HideSmall>
-            {chainId && NETWORK_LABELS[chainId] && (
-              <NetworkCard title={NETWORK_LABELS[chainId]}>{NETWORK_LABELS[chainId]}</NetworkCard>
-            )}
-          </HideSmall> */}
-      {/* {availableClaim && !showClaimPopup} */}
-      {/* <AccountElement active={!!account} style={{ pointerEvents: 'auto' }}>
-            {account && userEthBalance ? (
-              <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
-                {userEthBalance?.toSignificant(4)} TRX
-              </BalanceText>
-            ) : null}
-            <Web3Status />
-          </AccountElement>
-        </HeaderElement>
-        <HeaderElementWrap>
-          <Settings />
-          <Menu />
-        </HeaderElementWrap>
-      </HeaderControls> */}
-    </HeaderFrame>
+          </Navbar.Brand>
+          <div className="tokenname">
+            <span>TRX</span>
+            <span style={{ width: '30px', marginLeft: '7px' }}>
+              <img src={EthLogo} alt="" />
+            </span>
+          </div>
+          <NavDropdown title="" id="basic-nav-dropdowns">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          </NavDropdown>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mx-auto">
+              <Link to="/exchange" className={`${Style.link} nav-link`}>
+                Exchange
+              </Link>
+              <Link to="/liquidity" className={`${Style.link} nav-link`}>
+                Liquidity
+              </Link>
+              <Link to="/staking" className={`${Style.link} nav-link`}>
+                Staking
+              </Link>
+              <Link to="/nft" className={`${Style.link} nav-link`}>
+                NFT
+              </Link>
+              <NavDropdown title="" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <HeaderLinks>
+              <AccountElement active={!!account} style={{ pointerEvents: 'auto' }}>
+                {account && userEthBalance ? (
+                  <BalanceText style={{ flexShrink: 0 }} pl="0.75rem" pr="0.5rem" fontWeight={500}>
+                    {userEthBalance?.toSignificant(4)} TRX
+                  </BalanceText>
+                ) : null}
+                <Web3Status />
+              </AccountElement>
+              <Settings />
+            </HeaderLinks>
+            <Menu />
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
