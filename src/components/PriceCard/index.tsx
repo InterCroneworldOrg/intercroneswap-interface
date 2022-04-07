4aabcebd7585c05a6261204d1b2b837371f13978
Gabor Nagy <gaborka19890603@gmail.com>
2022-04-07 21:25:39 +0200

    Add priceCard for ICR price

---
+++ src/components/PriceCard/index.tsx
@@ -0,0 +1,22 @@
+import { useActiveWeb3React } from '../../hooks';
+import useUSDTPrice from '../../hooks/useUSDTPrice';
+import { AmountWrapper } from './styleds';
+import chessIcon from '../../assets/images/chessicon.png';
+import { ICR } from '../../constants/tokens';
+
+const PriceCard = () => {
+  const { account } = useActiveWeb3React();
+
+  const usdtPrice = useUSDTPrice(ICR);
+
+  return account && usdtPrice ? (
+    <AmountWrapper>
+      <span>
+        <img width={20} src={chessIcon} alt="" />
+      </span>
+      $ {usdtPrice?.adjusted.toFixed(ICR.decimals)} USD
+    </AmountWrapper>
+  ) : null;
+};
+
+export default PriceCard;
