21bef269cde72b4400afb4bb75eebc3ac8c15cdd
Gabor Nagy <gaborka19890603@gmail.com>
2022-04-08 20:01:39 +0200

    Fix meta and iswap list

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -2,10 +2,10 @@ import { ContextApi } from 'contexts/Localization/types'
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'PancakeSwap',
-  description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/hero.png',
+  title: 'IntercroneSwap',
+  description: 'Welcome to InterCroneWorld. The one stop swap, earn and gaming platform.',
+  // TODO add here when we have an ICR hero image
+  image: undefined,
 }
 
 export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
