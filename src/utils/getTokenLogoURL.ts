1da46d45bd88553f8a03e62e481c4cf580278ec4
Gabor Nagy <gaborka19890603@gmail.com>
2022-04-21 19:59:33 +0200

    Fix remaining issues at staking

--- src/utils/getTokenLogoURL.ts
+++ src/utils/getTokenLogoURL.ts
@@ -1,4 +1,13 @@
-const getTokenLogoURL = (address: string) =>
-  `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${address}/logo.png`
+import { tokens } from "../config/constants/tokenLists/iswap-default.tokenlist.json";
+
+const getTokenLogoURL = (address: string) => {
+  const logoFromDefault = defaultTokenLogoURL(address);
+  
+  return logoFromDefault ? logoFromDefault : `https://assets.trustwalletapp.com/blockchains/smartchain/assets/${address}/logo.png`
+}
+
+const defaultTokenLogoURL = (address: string) => {
+  return tokens.find(token => token.address === address)?.logoURI
+}
 
 export default getTokenLogoURL
