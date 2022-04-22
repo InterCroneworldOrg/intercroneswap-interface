1b18c8b223efb40b605c01aef6bd845ab6bc044e
Gabor Nagy <gaborka19890603@gmail.com>
2022-04-22 09:20:08 +0200

    Clean up bsc constant tokens

--- src/config/constants/swapWarningTokens.ts
+++ src/config/constants/swapWarningTokens.ts
@@ -1,18 +1,10 @@
 import { Token } from '@intercroneswap/v2-sdk'
 import tokens from 'config/constants/tokens'
 
-const { bondly, safemoon, itam, ccar, bttold } = tokens
-
 interface WarningTokenList {
   [key: string]: Token
 }
 
-const SwapWarningTokens = <WarningTokenList>{
-  safemoon,
-  bondly,
-  itam,
-  ccar,
-  bttold,
-}
+const SwapWarningTokens = <WarningTokenList>{}
 
 export default SwapWarningTokens
