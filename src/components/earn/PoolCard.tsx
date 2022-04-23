977b21cde622bb660a0d2bdba3fe5b1656af12a3
Gabor Nagy <gaborka19890603@gmail.com>
2022-04-23 15:06:43 +0200

    Add fixes to staking

--- src/components/earn/PoolCard.tsx
+++ src/components/earn/PoolCard.tsx
@@ -1,63 +1,62 @@
-import { ETHER, JSBI, Percent, TokenAmount } from '@intercroneswap/v2-sdk';
-import { useContext, useState } from 'react';
-import { Text, Button, LinkExternal} from '@pancakeswap/uikit'
-import { ThemeContext } from 'styled-components';
+import { ETHER, JSBI, Percent, TokenAmount } from '@intercroneswap/v2-sdk'
+import { useContext, useState } from 'react'
+import { Text, Button, LinkExternal } from '@pancakeswap/uikit'
+import { ThemeContext } from 'styled-components'
 import { PairState, usePair } from 'hooks/usePairs'
-import useTotalSupply from 'hooks/useTotalSupply';
+import useTotalSupply from 'hooks/useTotalSupply'
 import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import useBUSDPrice from 'hooks/useBUSDPrice'
 import { AutoColumn } from 'components/Layout/Column'
 import { CurrencyLogo } from 'components/Logo'
 import { AutoRow } from 'components/Layout/Row'
 import tokens from 'config/constants/tokens'
-import { Dots } from '../../views/Stake/styleds';
-import { StakingInfo } from '../../state/stake/hooks';
-import { useTokenBalance } from '../../state/wallet/hooks';
-import { isOneTokenWETH, unwrappedToken } from '../../utils/wrappedCurrency';
-import { LightCard } from '../Card';
-import DetailsDropdown from './DetailsDropdown';
-import { YEARLY_RATE } from '../../constants';
-import { AutoRowToColumn, ResponsiveSizedTextMedium, ResponsiveSizedTextNormal, ArrowWrapper } from './styleds';
-import StyledChevronUp from './ChevronUp';
-import StyledChevronDown from './ChevronDown';
+import { Dots } from '../../views/Stake/styleds'
+import { StakingInfo } from '../../state/stake/hooks'
+import { useTokenBalance } from '../../state/wallet/hooks'
+import { unwrappedToken } from '../../utils/wrappedCurrency'
+import { LightCard } from '../Card'
+import DetailsDropdown from './DetailsDropdown'
+import { YEARLY_RATE } from '../../constants'
+import { AutoRowToColumn, ResponsiveSizedTextMedium, ResponsiveSizedTextNormal, ArrowWrapper } from './styleds'
+import StyledChevronUp from './ChevronUp'
+import StyledChevronDown from './ChevronDown'
+import { DoubleTokenAmount, GetAmountInBUSD } from 'utils/tokens'
 
-const { icr: ICR, busd : BUSD } = tokens
+const { icr: ICR, busd: BUSD } = tokens
 
-const ZERO = JSBI.BigInt(0);
+const ZERO = JSBI.BigInt(0)
 
 interface PoolCardProps {
-  stakingInfo: StakingInfo;
-  address: string;
-  handleStake: (address: string, lpSupply?: TokenAmount, stakingInfo?: StakingInfo) => void;
-  handleHarvest: (address: string) => void;
-  toggleToken: boolean;
+  stakingInfo: StakingInfo
+  address: string
+  handleStake: (address: string, lpSupply?: TokenAmount, stakingInfo?: StakingInfo) => void
+  handleHarvest: (address: string) => void
+  toggleToken: boolean
 }
 
 export default function PoolCard({ stakingInfo, address, toggleToken, handleStake, handleHarvest }: PoolCardProps) {
-  const theme = useContext(ThemeContext);
-  const { account } = useActiveWeb3React();
+  const theme = useContext(ThemeContext)
+  const { account } = useActiveWeb3React()
 
-  const token0 = stakingInfo.tokens[0];
-  const token1 = stakingInfo.tokens[1];
-  const isOneTokenICR = token0 === ICR || token1 === ICR;
-  const [isWETH, weth] = isOneTokenWETH(token0, token1);
+  const token0 = stakingInfo.tokens[0]
+  const token1 = stakingInfo.tokens[1]
 
-  const currency0 = unwrappedToken(token0);
-  const currency1 = unwrappedToken(token1);
-  const [showMore, setShowMore] = useState(false);
-  const [pairState, pair] = usePair(currency0, currency1);
+  const currency0 = unwrappedToken(token0)
+  const currency1 = unwrappedToken(token1)
+  const [showMore, setShowMore] = useState(false)
+  const [pairState, pair] = usePair(currency0, currency1)
 
-  const LPSupply = useTokenBalance(account ?? undefined, pair?.liquidityToken ?? undefined);
-  const LPTotalSupply = useTotalSupply(pair?.liquidityToken);
+  const LPSupply = useTokenBalance(account ?? undefined, pair?.liquidityToken ?? undefined)
+  const LPTotalSupply = useTotalSupply(pair?.liquidityToken)
 
   // We create a new tokenAmaount as we get wrong pair from stakingInfo
-  const totalStakedAmount = pair ? new TokenAmount(pair?.liquidityToken, stakingInfo.totalStakedAmount.raw) : undefined;
-  const stakedAmount = pair ? new TokenAmount(pair?.liquidityToken, stakingInfo.stakedAmount.raw) : undefined;
+  const totalStakedAmount = pair ? new TokenAmount(pair?.liquidityToken, stakingInfo.totalStakedAmount.raw) : undefined
+  const stakedAmount = pair ? new TokenAmount(pair?.liquidityToken, stakingInfo.stakedAmount.raw) : undefined
 
-  const USDPrice = useBUSDPrice(ICR);
-  const USDPriceBNB = useBUSDPrice(weth);
-  const ratePerYear = stakingInfo.rewardForDuration.multiply(YEARLY_RATE);
-  const ratePerYearBUSD = ratePerYear && USDPrice?.quote(stakingInfo.rewardForDuration).multiply(YEARLY_RATE);
+  const USDPrice = useBUSDPrice(token0)
+  const earnedUSDPrice = useBUSDPrice(stakingInfo.earnedAmount.token)
+  const ratePerYear = stakingInfo.rewardForDuration.multiply(YEARLY_RATE)
+  const ratePerYearBUSD = ratePerYear && earnedUSDPrice?.quote(stakingInfo.rewardForDuration).multiply(YEARLY_RATE)
 
   // Check if the actual Token is ICR or WETH based
   const stakedInToken =
@@ -66,12 +65,8 @@ export default function PoolCard({ stakingInfo, address, toggleToken, handleStak
     !!LPSupply &&
     stakedAmount &&
     JSBI.greaterThan(LPTotalSupply?.raw, stakingInfo.stakedAmount.raw)
-      ? isOneTokenICR
-        ? pair?.getLiquidityValue(ICR, LPTotalSupply, stakedAmount, false)
-        : isWETH && weth
-        ? pair?.getLiquidityValue(weth, LPTotalSupply, stakedAmount, false)
-        : undefined
-      : undefined;
+      ? DoubleTokenAmount(pair.getLiquidityValue(token0, LPTotalSupply, stakedAmount, false))
+      : undefined
 
   const totalStakedInToken =
     !!pair &&
@@ -79,26 +74,18 @@ export default function PoolCard({ stakingInfo, address, toggleToken, handleStak
     !!LPSupply &&
     totalStakedAmount &&
     JSBI.greaterThan(LPTotalSupply?.raw, stakingInfo.totalStakedAmount.raw)
-      ? isOneTokenICR
-        ? pair?.getLiquidityValue(ICR, LPTotalSupply, totalStakedAmount, false)
-        : isWETH && weth
-        ? pair?.getLiquidityValue(weth, LPTotalSupply, totalStakedAmount, false)
-        : undefined
-      : undefined;
+      ? DoubleTokenAmount(pair.getLiquidityValue(token0, LPTotalSupply, totalStakedAmount, false))
+      : undefined
 
-  const valueOfTotalStakedAmountInBUSD = totalStakedInToken
-    ? isOneTokenICR
-      ? USDPrice?.quote(totalStakedInToken)
-      : USDPriceBNB?.quote(totalStakedInToken)
-    : undefined;
-  const valueOfEarnedAmountInBUSD = stakingInfo.earnedAmount && USDPrice?.quote(stakingInfo.earnedAmount);
+  const valueOfTotalStakedAmountInBUSD = GetAmountInBUSD(USDPrice, totalStakedInToken)
+  const valueOfEarnedAmountInBUSD = GetAmountInBUSD(earnedUSDPrice, stakingInfo.earnedAmount)
 
   const apr =
     ratePerYearBUSD &&
     valueOfTotalStakedAmountInBUSD &&
     JSBI.greaterThan(valueOfTotalStakedAmountInBUSD.numerator, ZERO)
       ? new Percent(ratePerYearBUSD.numerator, JSBI.multiply(valueOfTotalStakedAmountInBUSD.numerator, JSBI.BigInt(2)))
-      : 0;
+      : undefined
 
   return (
     <LightCard style={{ marginTop: '2px', margin: '0rem', padding: '1rem', background: theme.colors.background }}>
@@ -128,12 +115,13 @@ export default function PoolCard({ stakingInfo, address, toggleToken, handleStak
           <ResponsiveSizedTextMedium fontWeight="1.3rem">Earned / APY</ResponsiveSizedTextMedium>
           {toggleToken ? (
             <ResponsiveSizedTextNormal fontWeight="0.6rem" color={theme.colors.primary}>
-              {stakingInfo.earnedAmount.toSignificant()} <CurrencyLogo currency={ICR} size=".8rem" />/ {apr.toFixed(2)}{' '}
-              %
+              {stakingInfo.earnedAmount.toSignificant()} <CurrencyLogo currency={ICR} size=".8rem" />/{' '}
+              {apr ? `${apr.toFixed(2)} %` : '-'}{' '}
             </ResponsiveSizedTextNormal>
           ) : (
             <ResponsiveSizedTextNormal fontWeight="0.6rem" color={theme.colors.primary}>
-              {valueOfEarnedAmountInBUSD?.toFixed(2)} <CurrencyLogo currency={BUSD} size=".8rem" />/ {apr.toFixed(2)} %
+              {valueOfEarnedAmountInBUSD?.toFixed(2)} <CurrencyLogo currency={BUSD} size=".8rem" />/{' '}
+              {apr ? `${apr.toFixed(2)} %` : '-'}
             </ResponsiveSizedTextNormal>
           )}
         </AutoRowToColumn>
@@ -159,31 +147,25 @@ export default function PoolCard({ stakingInfo, address, toggleToken, handleStak
         </AutoRowToColumn>
         <AutoColumn justify="center" style={{ maxWidth: '25rem', width: '100%' }} gap="1rem">
           <AutoRow gap=".1rem">
-            <Button
-              padding="8px"
-              width="48%"
-              height={60}
-              onClick={() => handleHarvest(address)}
-            >
+            <Button padding="8px" width="48%" height={60} onClick={() => handleHarvest(address)}>
               <AutoColumn>
-                <ResponsiveSizedTextMedium fontWeight="0.8rem" color='rgb(44, 47, 54)'>Harvest</ResponsiveSizedTextMedium>
-                <ResponsiveSizedTextNormal fontWeight="0.5rem" color='rgb(44, 47, 54)'>
+                <ResponsiveSizedTextMedium fontWeight="0.8rem" color="rgb(44, 47, 54)">
+                  Harvest
+                </ResponsiveSizedTextMedium>
+                <ResponsiveSizedTextNormal fontWeight="0.5rem" color="rgb(44, 47, 54)">
                   {stakingInfo.earnedAmount?.greaterThan(0)
                     ? stakingInfo.earnedAmount?.toSignificant(4)
                     : 'Nothing to Harvest'}
                 </ResponsiveSizedTextNormal>
               </AutoColumn>
             </Button>
-            <Button
-              padding="8px"
-              width="48%"
-              height={60}
-              onClick={() => handleStake(address, LPSupply, stakingInfo)}
-            >
+            <Button padding="8px" width="48%" height={60} onClick={() => handleStake(address, LPSupply, stakingInfo)}>
               <AutoColumn>
-              <ResponsiveSizedTextMedium fontWeight="0.7rem" color='rgb(44, 47, 54)'>Stake / Unstake</ResponsiveSizedTextMedium>
+                <ResponsiveSizedTextMedium fontWeight="0.7rem" color="rgb(44, 47, 54)">
+                  Stake / Unstake
+                </ResponsiveSizedTextMedium>
                 {pairState === PairState.EXISTS ? (
-                  <ResponsiveSizedTextNormal fontWeight="0.5rem" color='rgb(44, 47, 54)'>
+                  <ResponsiveSizedTextNormal fontWeight="0.5rem" color="rgb(44, 47, 54)">
                     {LPSupply?.greaterThan(0) ? LPSupply?.toSignificant(4) : 'No liquidity'}
                   </ResponsiveSizedTextNormal>
                 ) : (
@@ -193,11 +175,11 @@ export default function PoolCard({ stakingInfo, address, toggleToken, handleStak
             </Button>
           </AutoRow>
           <ArrowWrapper clickable>
-            {showMore ? <StyledChevronUp
-              onClick={() => setShowMore(!showMore)} /> :
-              <StyledChevronDown
-              onClick={() => setShowMore(!showMore)} />
-            }
+            {showMore ? (
+              <StyledChevronUp onClick={() => setShowMore(!showMore)} />
+            ) : (
+              <StyledChevronDown onClick={() => setShowMore(!showMore)} />
+            )}
           </ArrowWrapper>
         </AutoColumn>
       </AutoRow>
@@ -213,5 +195,5 @@ export default function PoolCard({ stakingInfo, address, toggleToken, handleStak
         />
       )}
     </LightCard>
-  );
+  )
 }
