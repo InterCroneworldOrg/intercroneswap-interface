3fb8aa91db863f827563ed14fad22c1134be7b98
Gabor Nagy <gabor.nagy@partner.bmwgroup.com>
2022-07-05 09:24:07 +0200

    Work on mint infos and modal dialog

--- src/pages/Mint/index.tsx
+++ src/pages/Mint/index.tsx
@@ -1,26 +1,66 @@
 import '../../styles/mint.scss';
 import { Container } from 'react-bootstrap';
 import ApiBTC from '../../assets/images/ApiBTC.png';
-import apiICR from '../../assets/images/apiICR2.png';
+// import apiICR from '../../assets/images/apiICR2.png';
 import ApiTron from '../../assets/images/ApiTron.png';
 import Arrow from '../../assets/images/mintarrow.png';
+import { ArbiNFTInfo, useAbiBotActionHandlers, useAbiBotMintInfo, useAbiBotState } from '../../state/abibot/hooks';
+import { useCallback, useState } from 'react';
+import MintModal from './MintModal';
+
+// const getImageFromBaseURI = (uri: string): string => {
+//   return `https://gateway.pinata.cloud/${uri}1.png`;
+// };
 
 const MINT: React.FC = () => {
+  const mintInfos = useAbiBotMintInfo(['0x09D7BF82A90D5D4A5C5CB46C6D25989FC0373856']);
+  const [selectedBot, setSelectedBot] = useState<ArbiNFTInfo | undefined>(mintInfos[0]);
+  const { onUserInput, onTxHashChange } = useAbiBotActionHandlers();
+  const { typedValue } = useAbiBotState();
+
+  const [showMint, setShowMint] = useState(false);
+
+  const handleDismissMint = useCallback(() => {
+    setShowMint(false);
+    onUserInput('1');
+    onTxHashChange('');
+  }, [showMint, selectedBot]);
+
+  console.log('mintInfos: ', mintInfos, selectedBot);
+  const increaseMintAmount = useCallback(() => {
+    console.log('increasing mint amount');
+
+    onUserInput((Number(typedValue) + 1).toString());
+  }, [typedValue]);
+
+  const decreaseMintAmount = useCallback(() => {
+    if (Number(typedValue) > 1) {
+      onUserInput((Number(typedValue) - 1).toString());
+    }
+  }, [typedValue]);
+
   return (
     <>
       <h2 className="title">Minting now open !</h2>
       <Container>
+        <MintModal isOpen={showMint} onDismiss={handleDismissMint} mintInfo={selectedBot} />
         <div id="bot">
           <h3>Choose your Arbitrage Bot</h3>
           <div className="images">
-            <div className="imgparent">
-              <p>1204 Left</p>
-              <img src={apiICR} alt="" />
-              <div>
-                <img className="arrow" src={Arrow} alt="" />
-                <p>earn ICR</p>
-              </div>
-            </div>
+            {mintInfos &&
+              mintInfos.length > 0 &&
+              mintInfos.map((info, index) => (
+                <div key={index} className="imgparent">
+                  <p>{info.maxMintAmount - info.totalSupply} Left</p>
+                  <button onClick={() => setSelectedBot(info)}>
+                    <img src={ApiTron} alt="" />
+                  </button>
+                  <div>
+                    <img className="arrow" src={Arrow} alt="" />
+                    <p>earn ICR</p>
+                  </div>
+                </div>
+              ))}
             <div className="imgparent">
               <img src={ApiTron} alt="" />
             </div>
@@ -36,16 +76,16 @@ const MINT: React.FC = () => {
           </div>
           <div className="buymint">
             <p>
-              Max : <span style={{ color: '#E90A0E' }}>20</span>
+              Max : <span style={{ color: '#E90A0E' }}>{selectedBot?.maxMintPerTransaction}</span>
             </p>
             <div className="plusminus">
-              <button>-</button>
-              <input type="number" placeholder="1" />
-              <button>+</button>
+              <button onClick={decreaseMintAmount}>-</button>
+              <input type="number" placeholder={typedValue} value={typedValue} />
+              <button onClick={increaseMintAmount}>+</button>
             </div>
             <div className="mintnft">
-              <input type="text" placeholder="1500 TRX" />
-              <button>Mint NFT</button>
+              <input type="text" placeholder={selectedBot?.cost.toSignificant() ?? '-'} />
+              <button onClick={() => setShowMint(true)}>Mint NFT</button>
             </div>
             <p>Click on mint to Buy your NFT Token.</p>
           </div>
--- src/state/abibot/hooks.ts
+++ src/state/abibot/hooks.ts
@@ -1,6 +1,137 @@
-import { useSelector } from 'react-redux';
-import { AppState } from '..';
+import { useActiveWeb3React } from '../../hooks';
+import { useCallback, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, AppState } from '..';
+import { NEVER_RELOAD, useMultipleContractSingleData } from '../multicall/hooks';
+import { setAttemptingTxn, setTxHash, typeInput } from './actions';
+import { abi as AbiSwapICR } from '@intercroneswap/v2-abitragenft/build/AbiSwapICR.json';
+import { Interface } from 'ethers/lib/utils';
+import { ChainId, TokenAmount, WETH } from '@intercroneswap/v2-sdk';
+
+const AbiSwapICRInterface = new Interface(AbiSwapICR);
+
+export interface ArbiNFTInfo {
+  cost: TokenAmount;
+  mintAddress: string;
+  baseURI: string;
+  maxMintAmount: number;
+  maxMintPerTransaction: number;
+  totalSupply: number;
+}
 
 export function useAbiBotState(): AppState['abibot'] {
   return useSelector<AppState, AppState['abibot']>((state) => state.abibot);
+}
+
+export function useAbiBotActionHandlers(): {
+  onUserInput: (typedValue: string) => void;
+  onTxHashChange: (hashValue: string) => void;
+  onAttemptingTxn: (attmeping: boolean) => void;
+} {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const onFieldChange = useCallback(
+    (typedValue: string) => {
+      dispatch(typeInput({ typedValue }));
+    },
+    [dispatch],
+  );
+
+  const onAttemptingTxn = useCallback(
+    (attemptingTxn: boolean) => {
+      dispatch(setAttemptingTxn({ attemptingTxn }));
+    },
+    [dispatch],
+  );
+
+  const onTxHashChange = useCallback((hash: string) => dispatch(setTxHash({ txHash: hash })), [dispatch]);
+
+  return {
+    onUserInput: onFieldChange,
+    onAttemptingTxn,
+    onTxHashChange,
+  };
+}
+
+export function useAbiBotMintInfo(nftAddresses: string[]): ArbiNFTInfo[] {
+  const { chainId, account } = useActiveWeb3React();
+
+  const accountArg = useMemo(() => [account ?? undefined], [account]);
+
+  const costs = useMultipleContractSingleData(nftAddresses, AbiSwapICRInterface, 'cost', undefined, NEVER_RELOAD);
+
+  const maxMintAmounts = useMultipleContractSingleData(
+    nftAddresses,
+    AbiSwapICRInterface,
+    'maxMintAmount',
+    undefined,
+    NEVER_RELOAD,
+  );
+
+  const maxMintPerTransactions = useMultipleContractSingleData(
+    nftAddresses,
+    AbiSwapICRInterface,
+    'maxMintPerTransaction',
+    undefined,
+    NEVER_RELOAD,
+  );
+
+  const baseURIs = useMultipleContractSingleData(nftAddresses, AbiSwapICRInterface, 'baseURI', undefined, NEVER_RELOAD);
+
+  const totalSupplies = useMultipleContractSingleData(
+    nftAddresses,
+    AbiSwapICRInterface,
+    'totalSupply',
+    undefined,
+    NEVER_RELOAD,
+  );
+
+  return useMemo(() => {
+    if (!chainId) return [];
+
+    return nftAddresses.reduce<ArbiNFTInfo[]>((memo, nftAddress, index) => {
+      const costState = costs[index];
+      const maxMintAmountState = maxMintAmounts[index];
+      const maxMintPerTransactionState = maxMintPerTransactions[index];
+      const totalSupplyState = totalSupplies[index];
+      const baseURIState = baseURIs[index];
+
+      if (
+        costState &&
+        !costState.loading &&
+        maxMintAmountState &&
+        !maxMintAmountState.loading &&
+        maxMintPerTransactionState &&
+        !maxMintPerTransactionState.loading &&
+        totalSupplyState &&
+        !totalSupplyState.loading &&
+        baseURIState &&
+        !baseURIState.loading
+      ) {
+        if (
+          costState.error ||
+          maxMintAmountState.error ||
+          maxMintPerTransactionState.error ||
+          totalSupplyState.error ||
+          baseURIState.error
+        ) {
+          console.error('Failed to load arbi nft info');
+          return memo;
+        }
+
+        const ethTokenAmount = new TokenAmount(WETH[chainId ?? ChainId.MAINNET], costState?.result?.[0]);
+
+        memo.push({
+          cost: ethTokenAmount,
+          baseURI: baseURIState?.result?.[0],
+          mintAddress: nftAddress,
+          maxMintAmount: maxMintAmountState?.result?.[0]?.toNumber() ?? 0,
+          maxMintPerTransaction: maxMintPerTransactionState?.result?.[0]?.toNumber() ?? 0,
+          totalSupply: totalSupplyState?.result?.[0]?.toNumber() ?? 0,
+        });
+      }
+
+      return memo;
+    }, []);
+  }, [chainId, totalSupplies, maxMintAmounts, maxMintPerTransactions, costs, accountArg]);
 }
