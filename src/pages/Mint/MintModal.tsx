fdb52e9cfd06d100b47a5a431a3d1639857544e3
Manish Mandal <manish.mandal21@gmail.com>
2022-07-14 12:26:59 +0530

    Pool design with fixes

--- src/pages/Mint/MintModal.tsx
+++ src/pages/Mint/MintModal.tsx
@@ -115,7 +115,7 @@ export default function MintModal({ isOpen, onDismiss, mintInfo }: MintModalProp
   const confirmationContent = useCallback(() => {
     return (
       <ConfirmationModalContent
-        title="ArbiDex Mint"
+        title="Mint"
         onDismiss={onDismiss}
         topContent={modalHeader}
         bottomContent={modalBottom}
