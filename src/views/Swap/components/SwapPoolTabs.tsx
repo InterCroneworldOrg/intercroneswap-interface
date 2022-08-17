f04770f9b0efc472b87c941fa32e92caa788184a
Manish Mandal <manish.mandal21@gmail.com>
2022-08-17 11:24:56 +0530

    Swap page redesign

--- src/views/Swap/components/SwapPoolTabs.tsx
+++ src/views/Swap/components/SwapPoolTabs.tsx
@@ -1,43 +1,77 @@
 import styled from 'styled-components'
 import { darken } from 'polished'
 import { useRouter } from 'next/router'
+import GlobalSettings from 'components/Menu/GlobalSettings'
 
 const Tabs = styled.div`
   align-items: center;
   border-radius: 3rem;
   justify-content: space-evenly;
   display: flex;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `
 
 const StyledNavLink = styled.button`
   border-width: 0px;
-  background-color: ${({ theme }) => theme.colors.primaryBright};
+  background: linear-gradient(90deg, #ffb807 8.49%, #ffea00 100%);
+  color: #000000;
   align-items: center;
   justify-content: center;
-  height: 68px;
+  height: 55px;
   border-radius: 12px;
   outline: none;
   cursor: pointer;
   text-decoration: none;
-  color: ${({ theme }) => theme.colors.primary};
+  font-family: Poppins;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 28px;
+  flex-grow: 3;
+  flex-basis: 40%;
+  margin-right: 10px;
+  @media (max-width: 768px) {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 20px;
+  }
+`
+const StyledButton = styled.button`
+  border-width: 0px;
+  background: linear-gradient(90deg, #ffb807 8.49%, #ffea00 100%);
+  color: #000000;
+  align-items: center;
+  justify-content: center;
+  height: 55px;
+  border-radius: 12px;
+  outline: none;
+  cursor: pointer;
+  text-decoration: none;
   font-family: Poppins;
   font-style: normal;
   font-weight: 500;
   font-size: 18px;
   line-height: 28px;
   flex-grow: 1;
-
-  :hover,
-  :focus {
-    color: ${({ theme }) => darken(0.1, theme.colors.primary)};
+  flex-basis: 10%;
+  @media (max-width: 768px) {
+    width: 20%;
+    svg {
+      width: 18px;
+    }
   }
 `
+
 const StyledDisabledNavLink = styled.button`
   border-width: 0px;
-  background-color: transparent;
+  background: #3b3b3b;
+  border-radius: 12px;
   align-items: center;
   justify-content: center;
-  height: 68px;
+  height: 55px;
   border-radius: 15px;
   outline: none;
   cursor: pointer;
@@ -48,11 +82,20 @@ const StyledDisabledNavLink = styled.button`
   font-weight: 500;
   font-size: 18px;
   line-height: 28px;
-  flex-grow: 1;
+  flex-grow: 3;
+  flex-basis: 40%;
+  position: relative;
 
-  :hover,
-  :focus {
-    color: ${({ theme }) => darken(0.1, theme.colors.primary)};
+  margin-right: 10px;
+
+  p {
+    font-size: 8px;
+  }
+
+  @media (max-width: 768px) {
+    width: 100%;
+
+    margin-bottom: 20px;
   }
 `
 
@@ -72,8 +115,14 @@ export function SwapPoolTabs({ active }: { active: 'swap' | 'pool' }) {
     <Tabs style={{ marginTop: '20px', marginBottom: '20px' }}>
       {active === 'swap' && (
         <>
-          <StyledNavLink>Exchange</StyledNavLink>
-          <StyledDisabledNavLink onClick={() => router.push('/liquidity')}>Liquidity</StyledDisabledNavLink>
+          <StyledNavLink>Swap</StyledNavLink>
+
+          <StyledDisabledNavLink>
+            Professional <p>(Coming Soon)</p>
+          </StyledDisabledNavLink>
+          <StyledButton>
+            <GlobalSettings />
+          </StyledButton>
         </>
       )}
       {active === 'pool' && (
