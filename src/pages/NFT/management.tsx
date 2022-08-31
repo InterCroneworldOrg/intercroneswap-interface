0fc9e91701279ba96901c3adf1d72f5e9d67f146
Gabor Nagy <gabor.nagy@partner.bmwgroup.com>
2022-08-31 10:08:19 +0200

    Bind payment to the logic

--- src/pages/NFT/management.tsx
+++ src/pages/NFT/management.tsx
@@ -1,5 +1,5 @@
 import { GreyCard, LightCard } from '../../components/Card';
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { StyledHeading } from '../App';
 import { PageWrapper } from '../Stake/styleds';
 import CurrencyInputPanel from '../../components/CurrencyInputPanel';
@@ -11,25 +11,60 @@ import { AutoColumn } from '../../components/Column';
 import { useActiveWeb3React } from '../../hooks';
 import { unwrappedToken, wrappedCurrency } from '../../utils/wrappedCurrency';
 import { ICR } from '../../constants/tokens';
-import { AutoRow } from '../../components/Row';
+import { AutoRow, RowBetween } from '../../components/Row';
 import CurrencyLogo from '../../components/CurrencyLogo';
 import { BACKEND_URL } from '../../constants';
+import { Form } from 'react-bootstrap';
+import { ThemeContext } from 'styled-components';
 
 interface PayoutReport {
   holder_address: string;
   balance: number;
   payment: TokenAmount;
   payment_raw: string;
+  txID: string;
 }
 
 export const Management: React.FC = () => {
+  const theme = useContext(ThemeContext);
+
   const { chainId } = useActiveWeb3React();
   const [nftAddress, setNFTAddress] = useState('');
   const [paymentAmount, setPaymentAmount] = useState(1);
   const [currency, setCurrency] = useState<Currency>(ETHER);
   const [payoutReport, setPayoutReport] = useState<PayoutReport[]>([]);
+  const [dryRun, setDryRun] = useState(true);
 
-  const fetchData = async () => {
+  const triggerPayment = async () => {
+    const jsonData = {
+      nft_address: nftAddress,
+      token: currency.symbol,
+      amount: paymentAmount,
+      dry_run: dryRun,
+    };
+    const response = await fetch(`${BACKEND_URL}/abitrage/payout?chainId=${chainId}`, {
+      method: 'POST',
+      mode: 'cors',
+      body: JSON.stringify(jsonData),
+    });
+    const data = await response.json();
+    console.log(data.data);
+    const reports: PayoutReport[] = data.data.map((payoutreport: any) => {
+      return {
+        holder_address: payoutreport.holder_address,
+        balance: payoutreport.balance,
+        payment: new TokenAmount(
+          wrappedCurrency(currency, chainId) ?? ICR,
+          JSBI.BigInt(payoutreport.payment.numerator),
+        ),
+        payment_raw: payoutreport.payment_raw,
+        txID: payoutreport.txID,
+      };
+    });
+    setPayoutReport(reports);
+  };
+
+  const fetchReportData = async () => {
     const jsonData = {
       nft_address: nftAddress,
       token: currency.symbol,
@@ -89,14 +124,26 @@ export const Management: React.FC = () => {
               value={nftAddress}
             />
             <AutoColumn gap="20px">
-              <ButtonPrimary onClick={fetchData}>Payment report</ButtonPrimary>
-              <ButtonPrimary>Payment</ButtonPrimary>
+              <ButtonPrimary onClick={fetchReportData}>Payment report</ButtonPrimary>
+              <RowBetween>
+                <TYPE.yellow>
+                  When the test run is enabled, the transactions are not going to be signed and broadcasted
+                </TYPE.yellow>
+                <Form.Switch
+                  label="Test run"
+                  id="testrun"
+                  onChange={() => setDryRun(!dryRun)}
+                  defaultChecked={true}
+                  style={{ color: theme.text1 }}
+                />
+              </RowBetween>
+              <ButtonPrimary onClick={triggerPayment}>Payment</ButtonPrimary>
             </AutoColumn>
             {payoutReport && payoutReport.length > 0 && (
               <GreyCard style={{ marginTop: 20 }}>
                 <StyledHeading>Payout report</StyledHeading>
                 <AutoRow justify="space-between" padding="20px">
-                  <TYPE.white style={{ width: '15rem' }}>Holder address</TYPE.white>
+                  <TYPE.white style={{ width: '15rem' }}>TxID / Holder address</TYPE.white>
                   <TYPE.yellow>NFT tokens</TYPE.yellow>
                   <TYPE.white>Payout</TYPE.white>
                 </AutoRow>
@@ -104,11 +151,14 @@ export const Management: React.FC = () => {
                   return (
                     <GreyCard key={v.holder_address} padding="20px" style={{ margin: 14 }}>
                       <AutoRow justify="space-between">
-                        <TYPE.white style={{ width: '15rem' }}>{v.holder_address}</TYPE.white>
+                        <AutoColumn>
+                          <TYPE.white style={{ width: '15rem', wordBreak: 'break-all' }}>{v.txID}</TYPE.white>
+                          <TYPE.white style={{ width: '15rem' }}>{v.holder_address}</TYPE.white>
+                        </AutoColumn>
                         <TYPE.yellow>{v.balance}</TYPE.yellow>
                         <AutoColumn justify="center">
                           <CurrencyLogo currency={unwrappedToken(v.payment.token)} />
-                          <TYPE.white>{v.payment.toSignificant()}</TYPE.white>
+                          <TYPE.white>{v.payment_raw}</TYPE.white>
                         </AutoColumn>
                       </AutoRow>
                     </GreyCard>
