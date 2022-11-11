b3196629b7fcfba483c24b263a3303793bb5089d
Monahawk <34764873+Monahawk@users.noreply.github.com>
2022-11-11 12:15:28 +0100

    Add SST token to tron (#247)
    * Update tokens.ts
    
    * Fix prettier issues
    
    * Fix prettier again

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -34,6 +34,13 @@ export const WIN = new Token(ChainId.MAINNET, '0x74472E7D35395A6B5ADD427EECB7F4B
 export const LIVE = new Token(ChainId.MAINNET, '0xD829659F0F7661F29F12F07A5BE33C13B6C9DD59', 6, 'LIVE ', 'TRONbetLive');
 export const DICE = new Token(ChainId.MAINNET, '0x6CE0632A762689A207B9CCE915E93AA9596816CA', 6, 'DICE ', 'TRONbetDice');
 export const SM = new Token(ChainId.MAINNET, '0xAAB8C5F22E5E489A5824D8653F9BA68EB85FDE04', 8, 'SM ', 'SafeMoney');
+export const SST = new Token(
+  ChainId.MAINNET,
+  '0x0EFAC3802727C5F873B887E8119FE895B5156577',
+  8,
+  'SST ',
+  'SocialSwapToken',
+);
 export const EightEightEight = new Token(
   ChainId.MAINNET,
   '0x7818637395075A1AAA82927801DAB4117B99FC3F',
@@ -102,6 +109,7 @@ export const DefaultTokensMap: { [tokenSymbol: string]: Token } = {
   ['BBT']: BBT,
   ['COME']: COME,
   ['SM']: SM,
+  ['SST']: SST,
 };
 
 const tokens: Token[] = [
@@ -128,6 +136,7 @@ const tokens: Token[] = [
   KLV,
   Doge,
   TURU,
+  SST,
   BBT,
   COME,
   SM,
