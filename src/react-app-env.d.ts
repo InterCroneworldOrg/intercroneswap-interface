31dc50b660a35a8747713b5882dba51f624344b6
Partha Dhar <partha.dhar2008@gmail.com>
2023-12-10 01:33:24 +0530

    changed to lightnode

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -24,7 +24,7 @@ interface tronWeb {
   };
   fullNode?: {
     chainType?: '';
-    host?: 'https://fullnode.intercroneswap.com';
+    host?: 'https://imba-exchange.co/lightnode';
   };
 }
 interface Window {
