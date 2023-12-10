0f500be18f7a1fa6168ef0817f2121b6ee8c6de5
InterCrone <86061927+InterCroneworld@users.noreply.github.com>
2023-12-10 10:25:25 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,19 @@
-const express = require(`express`);
+const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000; // Heroku will need the PORT environment variable
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://imba-exchange.co');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
+
 app.listen(port, () => console.log(`App is live on port ${port}!`));
