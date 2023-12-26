4f87506e8ebead3cf9419ffe63a59b6bc1e62368
BigDealCompany <33265312+BigDealCompany@users.noreply.github.com>
2023-12-26 14:28:30 +0100

    update logo url

--- src/utils/tokenLogo.ts
+++ src/utils/tokenLogo.ts
@@ -6,14 +6,12 @@ const coinTopFormats = ['png', 'jpeg', 'jpg'];
 
 export const getTokenLogoURL = (address: string, allTokens: TokenList[]): string[] => {
   if (address.toLocaleLowerCase() === PLZ.address.toLowerCase())
-    return [
-      'https://static.tronscan.org/production/upload/logo/new/TYK71t3eD1pTxpkDp7gbqXM5DYfaVdfKjV.png?t=1668077389818',
-    ];
+    return ['https://static.tronscan.org/production/upload/logo/new/TYK71t3eD1pTxpkDp7gbqXM5DYfaVdfKjV.png'];
   const default_list_logo = allTokens
     .flatMap((tokens) => tokens.tokens)
     .find((token) => token.address.toLowerCase() === address.toLowerCase())?.logoURI;
-  const coin_top_main_url = `https://coin.top/production/upload/logo/${ethAddress.toTron(address)}.`;
-  const static_tronscan_url = `https://static.tronscan.org/production/upload/logo/new/${ethAddress.toTron(address)}.`;
+  const coin_top_main_url = `https://static.tronscan.org/production/upload/logo/${ethAddress.toTron(address)}.`;
+  const static_tronscan_url = `https://static.tronscan.org/production/upload/logo/${ethAddress.toTron(address)}.`;
   const allCoinTopFormats = coinTopFormats.map((format) => `${coin_top_main_url}${format}`);
   const newStaticFormats = coinTopFormats.map((format) => `${static_tronscan_url}${format}`);
   const allImageTypes = allCoinTopFormats.concat(newStaticFormats);
