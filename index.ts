b850d58e477eee5e24ce5cf96a3555275e0e5ee5
BigDealCompany <eder-michael@hotmail.com>
2024-01-27 15:29:26 +0100

    update cors

--- index.ts
+++ index.ts
@@ -5,7 +5,8 @@ const app = express();
 const port = process.env.PORT || 3000; // Heroku will need the PORT environment variable
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://lightnode.intercroneswap.com');
+  // Allow requests from any origin
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
